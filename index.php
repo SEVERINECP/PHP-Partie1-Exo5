e168d851d0b7d4fbe307f04b049ccdc44d07d22f
SEVERINECP <sevpicot@gmail.com>
2019-12-03 16:25:12 +0100

    PHPPartie1Exo5corrigé

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
 <?php
-$answer = 'yes';
-$answer = 'no';
+//Déclaration d'une seule variable est suffisante dans ce cas
+$answer = 'non';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>PHPPartie1Exo5IfElse</title>
   </head>
   <body>
-        <?php
-      if ($answer='yes'){
-        echo 'Vous avez répondu \'oui\'';
-      }else{
-        echo 'Vous avez répondu \'non\'';
-      }
-        ?>
+    <p>
+      <?php
+        if ($answer=='yes'){
+          echo 'Vous avez répondu \'oui\'';
+        }else{
+          echo 'Vous avez répondu \'non\'';
+        }
+      ?>
+    </p>
+    // On peut aussi coder ainsi, et c'est mieux! Car le texte est inscrit dans le html
+    <?php
+    // Déclaration de l'instruction if. Si la réponse est oui alors on affiche le message suivant(vous avez...)
+    if ($answer=='Oui'){
+      ?>
+      <p>Vous avez répondu oui</p>
+      <?php
+    // Sinon la réponse est non alors on affiche le message suivant (vous avez...)
+    }else{
+      ?>
+      <p>Vous avez répondu non</p>
+    <?php } ?>
   </body>
 </html>
